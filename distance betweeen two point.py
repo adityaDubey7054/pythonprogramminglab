af5ba4c103f7e36e521c0f4d2bb73b28dd51c00b
adityaDubey7054 <94119092+adityaDubey7054@users.noreply.github.com>
2022-05-31 15:50:50 +0530

    Add files via upload

---
+++ distance betweeen two point.py
@@ -0,0 +1,6 @@
+x1=eval(input("enter the no x1 "))
+x2=eval(input("enter the no x2")) 
+y1=eval(input("enter tne y1"))
+y2=eval(input('enter the no y2'))
+distance=((x2-x1)**2+(y2-y1)**2)**0.5
+print("result is ",distance)
